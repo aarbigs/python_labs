cef7a472a16413f28006e8ca35566673cc5cf521
aarbigs <bigelow_aaron@hotmail.com>
2019-03-19 16:36:34 -0600

    commit labs

--- 05_functions/05_01_tasks.py
+++ 05_functions/05_01_tasks.py
@@ -14,3 +14,19 @@ Write a script that completes the following tasks.
 # calls a function that determines whether the number is divisible by 4 or 7 exclusively
 
 # print the results
+
+num_input = int(input("Please enter a number betwee 1 and 1,000,000,000: "))
+
+def div_by_7_and_4(input):
+    if input % 7 == 0 and input % 4 == 0:
+        print("Divisible 7 and 4!")
+        return True
+
+def div_by_7_or_4(input):
+    if input % 7 == 0 or input % 4 == 0:
+        print("Divisible by 7 or 4!")
+        return True
+
+
+div_by_7_and_4(num_input)
+div_by_7_or_4(num_input)
--- 05_functions/05_02_rps.py
+++ 05_functions/05_02_rps.py
@@ -2,29 +2,52 @@
 Code a game of rock paper scissors.
 
 '''
+from random import randint
 
 
+player = int(input("Please enter a number: 0 = scissor, 1 = rock, 2 = paper: "))
 # function to get hand based on number
 # the function should take in a number and return the string representation of the hand
 def get_hand(hand):
     # 0 = scissor, 1 = rock, 2 = paper
-
+    if hand == 0:
+        print("scissor")
+        return "scissor"
+    elif hand == 1:
+        print("rock")
+        return "rock"
+    else:
+        print("paper")
+        return "paper"
     # for example if the variable hand is 0, it should return the string "scissor"
     pass
 
+get_hand(player)
+
+computer = randint(0, 2)
+print(str(computer))
+
+
 # function should take in two hands and return a string "You won!" or "You lost :(" or "You tied!"
 def determine_winner(computer, player):
+    if computer > player:
+        print("You lost :(")
+    elif computer < player:
+        print("You won!")
+    else:
+        print("You tied!")
     pass
 
 
-
-'''
-Start here
-'''
+determine_winner(computer, player)
 
 # take in a number 0-2 from the user that represents their hand
 
 # generate a random number 0-2 to use for the computer's hand
+
+
+
+
 
 # call the function get_hand to get the string representation of the user's hand
 
