42ed80a166762ed6fd0d336e4fee65de53a14c89
aarbigs <bigelow_aaron@hotmail.com>
2019-03-31 17:49:20 -0600

    commit labs 15

--- 15_variable_arguments/15_01_args.py
+++ 15_variable_arguments/15_01_args.py
@@ -2,3 +2,19 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def arg_function(*args):
+    for i in args:
+        print(i*i)
+
+
+arg_function(1, 2, 3, 4, 5)
+
+new_list = []
+def arg_function2(*args):
+    for arg in args:
+        new_list.append(arg)
+    print(new_list)
+
+
+arg_function2(1, 2, 3, 4, 5)
--- 15_variable_arguments/15_02_kwargs.py
+++ 15_variable_arguments/15_02_kwargs.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def kwargs_function(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v)
+
+
+kwargs_function(item_1="hello", item_2="world")
