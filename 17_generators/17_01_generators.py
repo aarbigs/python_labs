14d7c5186e01460ca86221e729cc732658d6d444
aarbigs <bigelow_aaron@hotmail.com>
2019-03-31 17:22:06 -0600

    commit labs 17

--- 17_generators/17_01_generators.py
+++ 17_generators/17_01_generators.py
@@ -3,3 +3,13 @@ Demonstrate how to create a generator object. Print the object to the console to
 Then iterate over the generator object and print out each item.
 
 '''
+
+gen_list = [i*2 for i in range(5)]
+print(type(gen_list))
+for i in gen_list:
+    print(i)
+
+gen = (i*2 for i in range(5))
+print(type(gen))
+for i in gen:
+    print(i)
