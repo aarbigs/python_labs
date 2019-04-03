4cdc160d8fdf2307e86c204b0184a5e7bc33f8f0
aarbigs <bigelow_aaron@hotmail.com>
2019-04-03 16:02:35 -0600

    commit lab 18

--- 18_lambdas/18_01_first_lambda.py
+++ 18_lambdas/18_01_first_lambda.py
@@ -2,3 +2,11 @@
 Write a lambda function that takes in 3 numbers and returns the sum of the numbers.
 
 '''
+from functools import reduce
+
+items = [1, 2, 3]
+#sum_3 = lambda x,y,z: x +y+z
+#print(sum_3(1,2,3))
+
+sum_map = reduce((lambda x,y: x+y), [1,2,3])
+print(sum_map)
--- 18_lambdas/18_02_return.py
+++ 18_lambdas/18_02_return.py
@@ -2,3 +2,7 @@
 Write a lambda function that does not take in an arguments but returns a value. Print the return value.
 
 '''
+
+import random
+rand_lamb = lambda: random.random()
+print(rand_lamb())
