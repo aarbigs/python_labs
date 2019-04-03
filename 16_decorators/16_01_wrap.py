c4e849a388e74103a703b852ff63bcbfbfaa2650
aarbigs <bigelow_aaron@hotmail.com>
2019-04-02 18:54:13 -0600

    commmit lab 16

--- 16_decorators/16_01_wrap.py
+++ 16_decorators/16_01_wrap.py
@@ -3,3 +3,28 @@ Write a decorator function that wraps text passed to it in a specified html tag.
 The user should be able to decide which tag to use.
 
 '''
+
+def p_decorate(func):
+   def func_wrapper(name):
+       return "<p>{0}</p>".format(func(name))
+   return func_wrapper
+
+def strong_decorate(func):
+    def func_wrapper(name):
+        return "<strong>{0}</strong>".format(func(name))
+    return func_wrapper
+
+def div_decorate(func):
+    def func_wrapper(name):
+        return "<div>{0}</div>".format(func(name))
+    return func_wrapper
+
+@div_decorate
+@p_decorate
+@strong_decorate
+def get_text(name):
+   return "lorem ipsum, {0} dolor sit amet".format(name)
+
+print(get_text("John"))
+
+
