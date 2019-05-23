38cb4cf8e0764a85c77cf1b2e53114b61b9a9063
aarbigs <bigelow_aaron@hotmail.com>
2019-05-22 20:48:18 -0600

    adding api labs

---
+++ python_apis_databases/apis/Exercise_01.py
@@ -0,0 +1,26 @@
+'''
+Using the requests package, make a GET request to the api behind this endpoint:
+
+    http://demo.codingnomads.co:8080/tasks_api/users
+
+Print out:
+
+    - the status code
+    - the encoding of the response
+    - the text of the response body
+
+
+
+'''
+
+import requests
+from pprint import pprint
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+respone = requests.get(base_url)
+
+pprint(respone)
+
+
+
